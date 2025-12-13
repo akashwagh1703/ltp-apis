0c48c6f6bd333fd5fc6a260819f68dd1e3994fe0
Akash Wagh <Akash.Wagh@esds.co.in>
2025-12-13 14:45:29 +0530

    new data

---
+++ database/seeders/SubscriptionPlanSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SubscriptionPlanSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $plans = [
+            [
+                'name' => 'Free Plan',
+                'type' => 'monthly',
+                'price' => 0.00,
+                'duration_days' => 30,
+                'is_active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Monthly Plan',
+                'type' => 'monthly',
+                'price' => 199.00,
+                'duration_days' => 30,
+                'is_active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Yearly Plan',
+                'type' => 'yearly',
+                'price' => 999.00,
+                'duration_days' => 365,
+                'is_active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        foreach ($plans as $plan) {
+            DB::table('subscription_plans')->updateOrInsert(
+                ['name' => $plan['name']],
+                $plan
+            );
+        }
+    }
+}
