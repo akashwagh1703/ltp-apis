a12760b95261d2b67c2d08b3ccd0422fc93a413b
Akash Wagh <Akash.Wagh@esds.co.in>
2025-12-16 20:40:44 +0530

    Get master

--- app/Http/Resources/PayoutResource.php
+++ app/Http/Resources/PayoutResource.php
@@ -10,15 +10,17 @@ class PayoutResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'payout_number' => $this->payout_number,
+            'payout_number' => $this->payout_number ?? 'PO' . str_pad($this->id, 6, '0', STR_PAD_LEFT),
             'period_start' => $this->period_start->format('Y-m-d'),
             'period_end' => $this->period_end->format('Y-m-d'),
             'total_bookings' => $this->total_bookings,
-            'total_revenue' => $this->total_revenue,
-            'commission_amount' => $this->commission_amount,
-            'payout_amount' => $this->payout_amount,
+            'total_revenue' => (float) ($this->total_amount ?? 0),
+            'commission_percentage' => (float) ($this->commission_percentage ?? 5.00),
+            'commission_amount' => (float) ($this->commission_amount ?? 0),
+            'payout_amount' => (float) ($this->settlement_amount ?? 0),
             'status' => $this->status,
             'processed_at' => $this->processed_at,
+            'paid_date' => $this->paid_date,
             'owner' => new OwnerResource($this->whenLoaded('owner')),
             'transactions' => PayoutTransactionResource::collection($this->whenLoaded('transactions')),
         ];
