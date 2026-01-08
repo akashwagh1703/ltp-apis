d575c649d4ae066c60f24e75f4e3b4ebae61107f
Akash Wagh <Akash.Wagh@esds.co.in>
2026-01-08 17:25:49 +0530

    Get master

---
+++ config/cors.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:3011',
+        'http://localhost:3000',
+        'http://143.110.183.5',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
