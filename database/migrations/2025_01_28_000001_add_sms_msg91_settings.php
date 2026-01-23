03af8162575c5b9328108112b2d0c49ce73f07b3
Akash Wagh <Akash.Wagh@esds.co.in>
2026-01-23 18:33:24 +0530

    Get V2 new

--- database/migrations/2025_01_28_000001_add_sms_msg91_settings.php
+++ database/migrations/2025_01_28_000001_add_sms_msg91_settings.php
@@ -11,15 +11,15 @@ return new class extends Migration
             // SMS & OTP Settings
             ['key' => 'sms_enabled', 'value' => 'false', 'type' => 'boolean', 'description' => 'Enable/Disable SMS notifications'],
             ['key' => 'default_otp_enabled', 'value' => 'true', 'type' => 'boolean', 'description' => 'Enable default OTP (999999) for testing'],
-            ['key' => 'default_otp', 'value' => '999999', 'type' => 'string', 'description' => 'Default OTP for testing'],
+            ['key' => 'default_otp', 'value' => '999999', 'type' => 'text', 'description' => 'Default OTP for testing'],
             
             // MSG91 Settings
-            ['key' => 'msg91_auth_key', 'value' => '', 'type' => 'string', 'description' => 'MSG91 Auth Key'],
-            ['key' => 'msg91_sender_id', 'value' => 'LTPLAY', 'type' => 'string', 'description' => 'MSG91 Sender ID'],
-            ['key' => 'msg91_otp_template_id', 'value' => '', 'type' => 'string', 'description' => 'MSG91 OTP Template ID'],
-            ['key' => 'msg91_booking_template_id', 'value' => '', 'type' => 'string', 'description' => 'MSG91 Booking Template ID'],
-            ['key' => 'msg91_cancel_template_id', 'value' => '', 'type' => 'string', 'description' => 'MSG91 Cancel Template ID'],
-            ['key' => 'msg91_dlt_entity_id', 'value' => '', 'type' => 'string', 'description' => 'MSG91 DLT Entity ID'],
+            ['key' => 'msg91_auth_key', 'value' => '', 'type' => 'text', 'description' => 'MSG91 Auth Key'],
+            ['key' => 'msg91_sender_id', 'value' => 'LTPLAY', 'type' => 'text', 'description' => 'MSG91 Sender ID'],
+            ['key' => 'msg91_otp_template_id', 'value' => '', 'type' => 'text', 'description' => 'MSG91 OTP Template ID'],
+            ['key' => 'msg91_booking_template_id', 'value' => '', 'type' => 'text', 'description' => 'MSG91 Booking Template ID'],
+            ['key' => 'msg91_cancel_template_id', 'value' => '', 'type' => 'text', 'description' => 'MSG91 Cancel Template ID'],
+            ['key' => 'msg91_dlt_entity_id', 'value' => '', 'type' => 'text', 'description' => 'MSG91 DLT Entity ID'],
         ];
 
         foreach ($settings as $setting) {
